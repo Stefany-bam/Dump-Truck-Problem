702011d5630cfaf610f9d66e218a90d8949c26cc
Abhay Navada <36285407+AbhayNavada@users.noreply.github.com>
2018-02-17 22:18:49 +0530

    Update dump_truck.c

--- dump_truck.c
+++ dump_truck.c
@@ -3,68 +3,74 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-#define n1 7
+//The 3 lines below mean that you can assign 7, 6 and 5 values to the loading time, weighing time and the travel time while taking in inputs
+#define n1 7		
 #define n2 6
 #define n3 5
-
 
 struct input_values{
 			int loading_time[n1];
 			int weighing_time[n2];
 			int travel_time[n3];
 }values;
+
 int a = 0, b = 0, c = 0;
 
-
 struct sys_state{
-			int lq;
-			int l;
-			int wq;
-			int w;
+			int lq;		//Loader Queue
+			int l;		//Loader
+			int wq;		//Weighing Queue
+			int w;		//Weighing Scale
 }state;
 
 
 struct dump_truck{
-			int id;
+			int id;			//Truck number
 			int loading_time;
 			int weighing_time;
 			int travel_time;
 }truck[7];
 
 
+//Represents a node in the Loader Queue, which is implemented as a Singly-linked list
 struct loader_queue{
-			int id;
+			int id;					//Truck number
 			struct loader_queue *next_ptr;
 }*l_start = NULL, *l_temp = NULL, *l_current = NULL;
 
 
+//This represents any of the 2 loader slots or the weighing scale
 struct slot{
-		int id;
+		int id;				//Truck number
 		struct slot *next_ptr;
 }*l1 = NULL, *l2 = NULL, *w1 = NULL;
 
 
+//Represents a node in the Weighing Queue, which is implemented as a Singly-linked list
 struct weighing_queue{
-			int id;
+			int id;					//Truck number
 			struct weighing_queue *next_ptr;
 }*w_start = NULL, *w_temp = NULL, *w_current = NULL;
 
 
+//The Future Event List stores all the upcoming events
+//There are 3 types of events namely End Loading(EL), End Weighing(EW) and Arrival at Loader Queue(ALQ)
 struct fel{
 		enum event_notices{ALQ, EL, EW} event;
-		int event_time;
+		int event_time;					//Time at which that event will be completed
 		int dt;
 		struct fel *next_ptr;
 }*fel_start = NULL, *fel_temp = NULL, *fel_current = NULL, *fel_prcd = NULL;
 
 
-int lbusy_time = 0;
-int wbusy_time = 0;
-int clock = 0;
+int lbusy_time = 0;		//Total Busy Time of both the loaders
+int wbusy_time = 0;		//Total Busy Time of the Weighing Scale
+int clock = 0;			//Maintains the clock time
 const char* eventnames[] = {"ALQ", "EL", "EW"};
 int prev_clock, prev_loaderstate, prev_weighscalestate;
 
 
+//This function is used give the loading time, weighing time and arrival time as inputs
 void init()
 {
 	int i;
@@ -139,7 +145,7 @@ void main()
 		l_temp = (struct loader_queue*)malloc(sizeof(struct loader_queue));
 		scanf("%d", &l_temp->id);
 		l_temp->next_ptr = NULL;
-		if(l_start == NULL)
+		if(l_start == NULL)		//If Loader Queue is empty
 		{
 			l_start = l_temp;
 			l_current = l_start;
@@ -151,14 +157,14 @@ void main()
 		}
 		l_temp = NULL;
 	}
-	if(state.l >= 1)
+	if(state.l >= 1)		//If there is a truck in loader 1
 	{
 		printf("Enter the ID of the truck present on loader L1:\t");
 		l1 = (struct slot*)malloc(sizeof(struct slot));
 		scanf("%d", &l1->id);
 		l1->next_ptr = NULL;
 	}
-	if(state.l == 2)
+	if(state.l == 2)		//If there are trucks in both the loaders
 	{
 		printf("Enter the ID of the truck present on loader L2:\t");
 		l2 = (struct slot*)malloc(sizeof(struct slot));
@@ -171,7 +177,7 @@ void main()
 		w_temp = (struct weighing_queue*)malloc(sizeof(struct weighing_queue));
 		scanf("%d", &w_temp->id);
 		w_temp->next_ptr = NULL;
-		if(w_start == NULL)
+		if(w_start == NULL)		//If the Weighing Queue is empty
 		{
 			w_start = w_temp;
 			w_current = w_start;
@@ -183,7 +189,7 @@ void main()
 		}
 		w_temp = NULL;
 	}
-	if(state.w == 1)
+	if(state.w == 1)		//If there is a truck in the Weighing Scale
 	{
 		printf("Enter the ID of the truck present on the weighing scale W1:\t");
 		w1 = (struct slot*)malloc(sizeof(struct slot));
@@ -191,40 +197,42 @@ void main()
 		w1->next_ptr = NULL;
 	}
 	init();
-	if(l1 != NULL)
+	if(l1 != NULL)		//If loader 1 is not empty
 	{
 		printf("Enter the loading time of DT%d:\t", l1->id);
 		printf("%d\n", truck[l1->id].loading_time = set_input(0));
 	}
-	if(l2 != NULL)
+	if(l2 != NULL)		//If loader 2 is not empty
 	{
 		printf("Enter the loading time of DT%d:\t", l2->id);
 		printf("%d\n", truck[l2->id].loading_time = set_input(0));
 	}
-	if(w1 != NULL)
+	if(w1 != NULL)		//If the weighing scale is not empty
 	{
 		printf("Enter the weighing time of DT%d:\t", w1->id);
 		printf("%d\n", truck[w1->id].weighing_time = set_input(1));
 	}
-	if(state.l >= 1)
+	
+	//Adding events to the Future Event List initially
+	if(state.l >= 1)		//If loader 1 is occupied by a truck
 	{
-		fel_temp = (struct fel*)malloc(sizeof(struct fel));
+		fel_temp = (struct fel*)malloc(sizeof(struct fel));		//Creating a temporary event
 		fel_temp->event = EL;
 		fel_temp->event_time = truck[l1->id].loading_time;
 		fel_temp->dt = l1->id;
 		fel_temp->next_ptr = NULL;
-		fel_start = fel_temp;
+		fel_start = fel_temp;						//Adding the temporary event to the actual list
 		fel_current = fel_start;
 		fel_temp = NULL;
 	}
-	if(state.l == 2)
+	if(state.l == 2)		//If both the loaders are occupied initially
 	{
 		fel_temp = (struct fel*)malloc(sizeof(struct fel));
 		fel_temp->event = EL;
 		fel_temp->event_time = truck[l2->id].loading_time;
 		fel_temp->dt = l2->id;
 		fel_temp->next_ptr = NULL;
-		if(fel_current->event_time <= fel_temp->event_time)
+		if(fel_current->event_time <= fel_temp->event_time)		//Arranging the events in increasing order of their finishing times
 		{
 			fel_current->next_ptr = fel_temp;
 			fel_current = fel_temp;
@@ -237,7 +245,7 @@ void main()
 		}
 		fel_temp = NULL;
 	}
-	if(state.w == 1)
+	if(state.w == 1)		//If a truck is present in the Weighing Scale
 	{
 		fel_temp = (struct fel*)malloc(sizeof(struct fel));
 		fel_temp->event = EW;
@@ -251,7 +259,7 @@ void main()
 		}
 		else
 		{
-			while(fel_current->event_time <= fel_temp->event_time && fel_current->next_ptr != NULL)
+			while(fel_current->event_time <= fel_temp->event_time && fel_current->next_ptr != NULL)		//Traversing to the place where we want to insert the new event in the actual list
 			{
 				fel_prcd = fel_current;
 				fel_current = fel_current->next_ptr;
@@ -306,9 +314,9 @@ void main()
 	fel_temp = NULL;
 	printf("\n\t\tBusy Loading Time:\t%d", lbusy_time);
 	printf("\n\t\tBusy Weighing Time:\t%d\n\n", wbusy_time);
-	while(clock <= 100 && fel_start != NULL)
+	while(clock <= 100 && fel_start != NULL)		//Printing the Future Event List till clock time is 100
 	{
-		if(fel_start->event == 0)
+		if(fel_start->event == 0)		//If the next event on the Future Event List is ALQ
 		{
 			prev_clock = clock;
 			prev_loaderstate = state.l;
@@ -330,7 +338,7 @@ void main()
 			}
 			else
 			{
-				if(l1 != NULL && l2 != NULL)
+				if(l1 != NULL && l2 != NULL)		//If both the loaders are occupied by trucks
 				{
 					l_start = (struct loader_queue*)malloc(sizeof(struct loader_queue));
 					l_start->id = fel_start->dt;
@@ -338,7 +346,7 @@ void main()
 					fel_start = fel_start->next_ptr;
 					state.lq++;
 				}
-				else if(l1 == NULL)
+				else if(l1 == NULL)		//If the first loader slot is empty
 				{
 					l1 = (struct slot*)malloc(sizeof(struct slot));
 					l1->id = fel_start->dt;
@@ -446,7 +454,7 @@ void main()
 			printf("\n\t\tBusy Loading Time:\t%d", lbusy_time);
 			printf("\n\t\tBusy Weighing Time:\t%d\n\n", wbusy_time);		
 		}
-		else if(fel_start->event == 1)
+		else if(fel_start->event == 1)			//If the next event on the Future Event List is EL
 		{
 			prev_clock = clock;
 			prev_loaderstate = state.l;
@@ -454,7 +462,7 @@ void main()
 			clock = fel_start->event_time;
 			printf("Enter the weighing time for DT%d:\t", fel_start->dt);
 			printf("%d\n", truck[fel_start->dt].weighing_time = set_input(1));
-			if(w_start != NULL)
+			if(w_start != NULL)		//If the Weighing Queue is not empty
 			{
 				w_temp = (struct weighing_queue*)malloc(sizeof(struct weighing_queue));
 				w_current = w_start;
@@ -470,7 +478,7 @@ void main()
 			}
 			else
 			{
-				if(w1 != NULL)
+				if(w1 != NULL)		//If the Weighing Scale is occupied
 				{
 					w_temp = (struct weighing_queue*)malloc(sizeof(struct weighing_queue));
 					w_temp->id = fel_start->dt;
@@ -523,7 +531,7 @@ void main()
 				l1 = NULL;
 			else if(l2 != NULL && l2->id == fel_start->dt)
 				l2 = NULL;
-			if(l_start != NULL && l1 == NULL)
+			if(l_start != NULL && l1 == NULL)		//If the first loader is empty and at least one truck is waiting in the Loader Queue
 			{
 				l1 = (struct slot*)malloc(sizeof(struct slot));
 				l1->id = l_start->id;
@@ -563,7 +571,7 @@ void main()
 				state.l++;
 				fel_temp  = NULL;
 			}
-			else if(l_start != NULL && l2 == NULL)
+			else if(l_start != NULL && l2 == NULL)		//If the second loader is vacant and at least one truck is waiting in the Loader Queue
 			{
 				l2 = (struct slot*)malloc(sizeof(struct slot));
 				l2->id = l_start->id;
@@ -637,7 +645,7 @@ void main()
 			printf("\n\t\tBusy Loading Time:\t%d", lbusy_time);
 			printf("\n\t\tBusy Weighing Time:\t%d\n\n", wbusy_time);
 		}
-		else
+		else		//If the next event in the Future Event List is EW
 		{
 			prev_clock = clock;
 			prev_loaderstate = state.l;
@@ -677,7 +685,7 @@ void main()
 			fel_temp = NULL;
 			fel_start = fel_start->next_ptr;
 			fel_current = fel_start;
-			if(w_start == NULL)
+			if(w_start == NULL)		//If the Weiging Queue is empty
 			{
 				w1 = NULL;
 				state.w = 0;
